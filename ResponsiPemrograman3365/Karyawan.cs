15fec82740ed29d741551cea68cd62342c1fdafd
surhes <surhes10@gmail.com>
2021-04-13 14:39:11 +0700

    Add project files.

---
+++ ResponsiPemrograman3365/Karyawan.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman3365
+{
+    class Karyawan
+    {
+        public int nim { get; set; }
+        public string nama { get; set; }
+        public int gaji { get; set; }
+        public int naikGaji { get; set; }
+
+        public Karyawan(int nim, string nama, int gaji)
+        {
+            this.nim = nim;
+            this.nama = nama;
+
+            if (gaji < 0)
+            {
+                this.gaji = 0;
+            }
+            else
+            {
+                this.gaji = gaji;
+            }
+
+            double tmp = 0;
+            tmp = 0.1 * gaji;
+            naikGaji = Convert.ToInt32(tmp) + gaji;
+        }
+
+    }
+}
+
---
+++ ResponsiPemrograman3365/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman3365
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan1 = new Karyawan(20113365, "surhes", 7000000);
+            Karyawan karyawan2 = new Karyawan(20113999, "sasuke ", 6000000);
+            Karyawan karyawan3 = new Karyawan(20113888, "naruto", 5000000);
+
+
+            Console.WriteLine("No.\t NIK \t   \t Nama \t   \t\t  Gaji      ");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("1.   {0} \t   \t {1} \t   \t {2}     ", karyawan1.nim, karyawan1.nama, karyawan1.gaji);
+            Console.WriteLine("2.   {0} \t   \t {1} \t   \t {2}     ", karyawan2.nim, karyawan2.nama, karyawan2.gaji);
+            Console.WriteLine("3.   {0} \t   \t {1} \t   \t {2}     ", karyawan3.nim, karyawan3.nama, karyawan3.gaji);
+
+
+            Console.WriteLine("\n Alhamdulillah naek gaji 10 % \n");
+
+
+            Console.WriteLine("No. \t NIK \t   \t Nama \t   \t\t  Gaji      ");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("1.   {0} \t   \t {1} \t   \t {2}     ", karyawan1.nim, karyawan1.nama, karyawan1.naikGaji);
+            Console.WriteLine("2.   {0} \t   \t {1} \t   \t {2}     ", karyawan2.nim, karyawan2.nama, karyawan2.naikGaji);
+            Console.WriteLine("3.   {0} \t   \t {1} \t   \t {2}     ", karyawan3.nim, karyawan3.nama, karyawan3.naikGaji);
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
+    
